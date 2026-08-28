3f30a54e2ac6b11765f98a7b6bf1a36ca2e88ee1
Gautam-Arya-007 <gautamarya9256@gmail.com>
2026-08-28 17:57:59 +0530

    Progress

---
+++ Python Playground/practice.py
@@ -0,0 +1,9 @@
+#Relational Operators
+Num1 = int(input("Enter the First Number :"))
+Num2 = int(input("Enter the Second Number :"))
+if(Num1>Num2):
+    print("First Number is greater than the Second One")
+elif(Num1<Num2):
+    print("First Number is smaller than the Second One")
+else:
+    print("Both Numbers are equal")
